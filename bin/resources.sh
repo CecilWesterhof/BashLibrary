55e40fd82dc434e7ec3f9b382380344bb52597bf
Cecil Westerhof <bash@decebal.nl>
2016-01-30 13:44:29 +0100

    Made several changes to make it more useful for other users. - It now works without the Bash library installed. - It is now possible to overwrite already saved data. - When data is already saved and not to be overwritten, just give a message and quit. - It is now possible to save ‘every’ second instead of once a day. - It is possible to show the raw output (-k) from df besides human readable. - Removed usage of --output-type, because this did not work everywhere. - There is an option to only output the values and not save them to the database.

--- bin/resources.sh
+++ bin/resources.sh
@@ -1,20 +1,63 @@
 #!/usr/bin/env bash
 
-source /usr/local/bash/BASHInitialisation.sh
+# Take care of the possibility that the Bash library is not installed
+declare -r BASH_LIB=/usr/local/bash/BASHInitialisation.sh
+if [ -f "${BASH_LIB}" -a -r "${BASH_LIB}" ] ; then
+    source "${BASH_LIB}"
+else
+    printf "No Bash library\n"
+    function fatal {
+        echo ${1}
+        exit 1
+    }
+fi
 
 set -o errexit
 set -o nounset
 
-declare -r  SCRIPT_NAME="${0##*/}"
+declare    ONLY_OUTPUT="N"
+declare    OVERWRITE="N"
+declare    RAW_OUTPUT="N"
+declare    RUN_OFTEN="N"
+declare -r SCRIPT_NAME="${0##*/}"
+declare -r USAGE="${SCRIPT_NAME} [--only-output] [--overwrite] [--raw-output] [--run-often] <DATABASE>"
 
-if [[ ${#} -ne 1 ]] ; then
-    fatal "${SCRIPT_NAME} <DATABASE>"
+if [[ "${#}" -ge 1 ]] && [[ "${1}" == '--only-output' ]]; then
+    ONLY_OUTPUT="T" ; shift
+fi
+readonly ONLY_OUTPUT
+if [[ "${#}" -ge 1 ]] && [[ "${1}" == '--overwrite' ]]; then
+    OVERWRITE="T" ; shift
+fi
+readonly OVERWRITE
+if [[ "${#}" -ge 1 ]] && [[ "${1}" == '--raw-output' ]]; then
+    RAW_OUTPUT="T" ; shift
+fi
+readonly RAW_OUTPUT
+if [[ "${#}" -ge 1 ]] && [[ "${1}" == '--run-often' ]]; then
+    RUN_OFTEN="T" ; shift
+fi
+readonly RUN_OFTEN
+if [[ "${#}" -ne 1 ]] ; then
+    fatal "${USAGE}"
     return
 fi
 
 
 declare -r DATABASE="${1}" ; shift
-declare -r DATE=$(date +%F)
+if [[ "${RUN_OFTEN}" == "T" ]] ; then
+    declare -r DATE=$(date +%F_%T)
+else
+    declare -r DATE=$(date +%F)
+fi
+declare -r DELETE_SAVED="
+    DELETE FROM directoryUsage
+    WHERE date = '${DATE}'
+    ;
+    DELETE FROM partitionUsage
+    WHERE date = '${DATE}'
+    ;
+"
 declare -r DIR_INSERT="
     INSERT INTO directoryUsage
     (date, directory, used)
@@ -25,6 +68,11 @@ declare -r DIR_INSERT="
 declare -r DIR_OUTPUT="%-12s %4s\n"
 declare -r DIR_SELECT="SELECT value FROM variables WHERE name = 'directories';"
 declare -r DIRS=$(sqlite3 ${DATABASE} <<<${DIR_SELECT})
+if [[ "${RAW_OUTPUT}" == "T" ]] ; then
+    declare -r OUTPUT_TYPE="-k"
+else
+    declare -r OUTPUT_TYPE="-h"
+fi
 declare -r PART_INSERT="
     INSERT INTO partitionUsage
     (date, partition, size, used, available, percentUsed)
@@ -35,9 +83,21 @@ declare -r PART_INSERT="
 declare -r PART_OUTPUT="%-8s %4s %4s %4s %4s\n"
 declare -r PART_SELECT="SELECT value FROM variables where name = 'partitions';"
 declare -r PARTITIONS="$(sqlite3 ${DATABASE} <<<${PART_SELECT})"
+declare -r WAS_ALREADY_SAVED="
+    SELECT date
+    FROM   directoryUsage
+    WHERE  date = '${DATE}'
+    UNION
+    SELECT date
+    FROM   partitionUsage
+    WHERE  date = '${DATE}'
+    ;
+"
 
 declare available
 declare directory
+declare filesystem
+declare isSaved
 declare line
 declare partition
 declare percentUsed
@@ -45,27 +105,44 @@ declare size
 declare used
 
 
+if [[ "${ONLY_OUTPUT}" != "T" ]] ; then
+    if [[ "${OVERWRITE}" == "T" ]] ; then
+        printf "${DELETE_SAVED}" | sqlite3 "${DATABASE}"
+    else
+        isSaved=$(printf "${WAS_ALREADY_SAVED}" | sqlite3 "${DATABASE}")
+        if [[ "${isSaved}" != "" ]] ; then
+            printf "There is already data for %s.\nQuiting\n" "${DATE}"
+            exit
+        fi
+    fi
+fi
+
+
 while read used directory ; do
     printf "${DIR_OUTPUT}"           "${directory}" "${used}"
-    printf "${DIR_INSERT}" "${DATE}" "${directory}" "${used}" | \
-        sqlite3 "${DATABASE}"
+    if [[ "${ONLY_OUTPUT}" != "T" ]] ; then
+        printf "${DIR_INSERT}" "${DATE}" "${directory}" "${used}" | \
+            sqlite3 "${DATABASE}"
+    fi
 done < <(du -hs ${DIRS})
 
 echo
 
 {
     read line
-    while read partition size used available percentUsed ; do
+    while read filesystem size used available percentUsed partition ; do
         printf                                  \
             "${PART_OUTPUT}"                    \
             "${partition}"                      \
             "${size}" "${used}" "${available}"  \
             "${percentUsed}"
-        printf                                  \
-            "${PART_INSERT}"                    \
-            "${DATE}"                           \
-            "${partition}"                      \
-            "${size}" "${used}" "${available}"  \
-            "${percentUsed}"                    | sqlite3 "${DATABASE}"
+        if [[ "${ONLY_OUTPUT}" != "T" ]] ; then
+            printf                                  \
+                "${PART_INSERT}"                    \
+                "${DATE}"                           \
+                "${partition}"                      \
+                "${size}" "${used}" "${available}"  \
+                "${percentUsed}"                    | sqlite3 "${DATABASE}"
+        fi
     done
-} < <(df -h --output=target,size,used,avail,pcent ${PARTITIONS})
+} < <(df "${OUTPUT_TYPE}" ${PARTITIONS})
