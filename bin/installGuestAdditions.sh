372cb896d9ac3e023c329feb1cfbb3c3ebbe82e0
Cecil Westerhof <bash@decebal.nl>
2017-02-03 11:02:39 +0100

    Started to work with distribution depended code Check that it is executed as root. Added a ‘succes’ message.

--- bin/installGuestAdditions.sh
+++ bin/installGuestAdditions.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# For this script to work you should have installed:
-# - headers current running kernel
-# - gcc
-
-
 # An error should terminate the script
 set -o errexit
 set -o nounset
@@ -13,12 +8,38 @@ set -o nounset
 # Always define all used variables
 # I use uppercase for readonly variables
 declare -r COMMAND=VBoxLinuxAdditions.run
+declare -r DISTRIBUTION=$(awk -F = '{
+                              if ($1 == "ID") {
+                                  print $2
+                              }
+                          }' /etc/os-release 2>/dev/null)
 declare -r MEDIA=/media/cdrom
+
 # This is set in the script itself. So no -r and a readonly in the script.
 declare    IS_MOUNTED=no
 
+if [[ $(id --user) != 0 ]] ; then
+    printf "Need to be root to execute this!!!\n"
+    exit 1
+fi
+
 
 # main code
+# Make sure the necessary stuff is installed
+case "${DISTRIBUTION}" in
+    debian|ubuntu)
+	apt-get update
+	apt-get upgrade
+	apt-get install build-essential module-assistant
+	m-a prepare
+        ;;
+    *)
+        printf "Unknown distibution (${DISTRIBUTION})\n"
+        printf "You should have installed at least the headers for the current running kernel and gcc\n"
+        printf "If not: terminate this script\n"
+        sleep 30
+        ;;
+esac
 if grep -q "${MEDIA}" /proc/mounts; then
     IS_MOUNTED=yes
 fi
@@ -26,8 +47,9 @@ readonly IS_MOUNTED
 if [[ "${IS_MOUNTED}" != yes ]] ; then
     mount "${MEDIA}"
 fi
-sudo sh "${MEDIA}/${COMMAND}" || printf "\n\n%s gave back %s\n" "${COMMAND}" "${?}"
+sh "${MEDIA}/${COMMAND}" || printf "\n\n%s gave back %s\n" "${COMMAND}" "${?}"
 # You need to reboot after this, but I find it still important to clean up
 if [[ "${IS_MOUNTED}" != yes ]] ; then
     umount "${MEDIA}"
 fi
+printf "Successful installed guest additions.\nYou should reboot your guest now.\n"
