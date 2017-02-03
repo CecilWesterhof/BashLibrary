3e96647a11d239768e86526663805b55892c6f45
Cecil Westerhof <bash@decebal.nl>
2017-02-03 11:22:39 +0100

    Added getDuration

--- time.sh
+++ time.sh
@@ -7,6 +7,29 @@
 # Functions                                                                    #
 ################################################################################
 
+# Usage: getDuration <SECONDS>
+# Needed
+# - BASH functions
+#   - fatal
+function getDuration {
+    if [[ ${#} -ne 1 ]] ; then
+        fatal "${FUNCNAME} <SECONDS>"
+        return
+    fi
+
+    local hours
+    local minutes
+    local seconds
+    local tmp
+
+    tmp=${1}; shift
+    seconds=$((tmp % 60))
+    tmp=$((tmp / 60))
+    minutes=$((tmp % 60))
+    hours=$((tmp / 60))
+    printf "%d:%02d:%02d\n" ${hours} ${minutes} ${seconds}
+}
+
 # Usage: getSeconds <TIME_STRING>
 # Needed
 # - BASH functions
@@ -17,11 +40,13 @@ function getSeconds {
         return
     fi
 
+    declare -r TIME_FORMAT='Time has to be in the format xx:xx or xx:xx:xx'
+
     declare -i seconds=0
     declare    timeString=${1}; shift
 
     if [[ ${#timeString} -ne 5 ]] && [[ ${#timeString} -ne 8 ]] ; then
-        printf "Time has to be in the format xx:xx or xx:xx:xx\n" >&2
+        printf "${TIME_FORMAT}\n" >&2
         return 1
     fi
     while [[ ${#timeString} -gt 0 ]] ; do
