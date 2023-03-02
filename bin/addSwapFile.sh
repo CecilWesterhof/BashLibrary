681a8093ec0ed7ce23077b0151d8e79345bc278c
Cecil Westerhof <bash@decebal.nl>
2023-03-02 12:30:52 +0100

    Changes to addSwapFile.sh: - Added comments - Added checks getFreeGB - giveError:   - To stderr instead of stdout   - Gets also an error code
    Make it compatible with shellcheck.

--- bin/addSwapFile.sh
+++ bin/addSwapFile.sh
@@ -1,48 +1,84 @@
 #!/usr/bin/env bash
 
+: <<'###BLOCK-COMMENT'
+Try to create and use new swapfile.
+Parameters:
+- swapfile to create
+- number of GB (an integer)
+
+Error codes:
+ 1 user is not root
+ 2 wrong parameters
+ 3 swapfile already exists
+ 4 not enough space on device
+ 5 filesystem is not on /dev
+###BLOCK-COMMENT
+
+# I want to exit on an error and an unset variable is an error
 set -o errexit
 set -o nounset
 
 
-
 function getFreeGB {
-    df -BG $(dirname $1) |  \
-        tail -n 1        |  \
-        awk '{print substr($4, 1, length($4) - 1)}'
+    fields="$(df -BG -T "$(dirname "$1")" | awk '
+        END { print dev, fs, gb }
+        {
+            dev = $1
+            fs  = $2
+            gb  = 0 + $5
+        }
+    ')"
+    read -r dev fs gb <<< "${fields}"
+    # Check $dev starts with /dev/
+    if [[ ! $dev =~ ^/dev/.* ]]; then
+        giveError "File system is not on /dev ($dev)" 5
+    fi
+    if [[ $fs == btrfs ]] ; then
+        echo "This script is not guaranted to work with btrfs" >&2
+    fi
+    echo "$gb"
 }
 
 function giveError {
-    echo $1
-    exit 1
+    echo "$1" >&2
+    exit "$2"
 }
 
 
-declare -r _scriptName=$(basename ${0})
+# Get the parameters for the script
+declare _scriptName
+_scriptName=$(basename "$0")
+readonly _scriptName
 if [[ $(id --user) -ne 0 ]] ; then
-    giveError "${_scriptName} should be run as root"
+    giveError "${_scriptName} should be run as root" 1
 fi
 if [[ $# -ne 2 ]] ; then
-    giveError "ERROR: ${_scriptName} SWAPFILE GB"
+    giveError "ERROR: ${_scriptName} SWAPFILE GB" 2
 fi
-declare -r  _swapFile=$1
-declare -ir _gb=$2
+declare -r  _swapFile="$1"
+declare -ir _gb="$2"
 
 if [[ -e ${_swapFile} ]] ; then
-    giveError "${_swapFile} already exists"
+    giveError "${_swapFile} already exists" 3
 fi
-declare -ir _gbFree=$(getFreeGB ${_swapFile})
+declare -i  _gbFree
+_gbFree=$(getFreeGB "${_swapFile}")
+readonly _gbFree
 # Make sure swapfile is less as a quarter of free space
-if [[ ${_gbFree} -le $(($_gb * 4)) ]] ; then
-    giveError "Not enough space for swap file ($_gb, ${_gbFree})"
+if (( _gbFree <= _gb * 4 )) ; then
+    giveError "Not enough space for swap file ($_gb, ${_gbFree})" 4
 fi
+
 echo Current Swap:
 swapon
 echo
 date "+%T: Creating ${_swapFile}"
-dd if=/dev/zero of=${_swapFile} bs=1024 count=$(($_gb * 1024 ** 2))
+dd if=/dev/zero of="${_swapFile}" bs=1024 count=$((_gb * 1024 ** 2))
 echo
-chmod 600 ${_swapFile}
-mkswap    ${_swapFile}
-swapon    ${_swapFile}
+# @@@@ check for right permission and ownership?
+chown :disk "${_swapFile}"
+chmod 600   "${_swapFile}"
+mkswap      "${_swapFile}"
+swapon      "${_swapFile}"
 echo New Swap:
 swapon
